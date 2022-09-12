9fc410a668d775d7f2f39489b3da1461a58c0c0b
Justsosoly <liaoyigenius@gmail.com>
2022-09-12 09:44:45 +0800

    0912

--- Myleecode/src/Mytest/MySort.java
+++ Myleecode/src/Mytest/MySort.java
@@ -1,5 +1,61 @@
 package Mytest;
 
 public class MySort {
+	
+	
+	public static void quickSort(int[]arr,int low,int high)
+	{
+		
+		
+		
+	    if(low>high) return ;
+	    int index=arr[low];
+	    int i=low;
+		int j=high;
+		while(i<j)
+		{
+			while(i<j&&index<=arr[j])
+			{
+				j--;
+			}
+			
+			while(i<j&&index>=arr[i])
+			{
+				i++;
+			}
+			
+			if(i<j)
+			{
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+			
+		}
+		arr[low]=arr[i];
+		arr[i]=index;
+		
+		quickSort(arr,low,j-1);
+		quickSort(arr,j+1,high);
+		
+		
+		
+		
+	}
+	
+	
+	
+	public static void main(String args[])
+	{
+		int arr[]= {6,1,2,7,9,3,4,5,10,8};
+		int low=0;
+		int high=arr.length-1;
+		quickSort(arr,low,high);
+		
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+		
+	}
+	
 
 }
--- Myleecode/src/Mytest/SortList.java
+++ Myleecode/src/Mytest/SortList.java
@@ -1,5 +1,110 @@
 package Mytest;
 
-public class SortList {
+import java.io.*;
+import java.math.*;
+
+import java.util.*;
+
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class SortList {
+
+    /*
+     * Complete the 'bigSorting' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY unsorted as parameter.
+     */
+
+    public static List<String> bigSorting(List<String> unsorted) {
+    // Write your code here
+    
+        int length=unsorted.size();
+        int low=0;
+        int high=length-1;
+       quickSorting(unsorted,low,high);
+    
+         for(int i=0;i<length;i++)
+            System.out.println(unsorted.get(i));
+    
+    
+     return unsorted;
+    }
+    
+    public static void quickSorting(List<String> unsorted,int low,int high)
+    {
+    
+        
+       if(low>high) return ;
+     
+       int i=low;
+       int j=high;
+       BigInteger index=new BigInteger(unsorted.get(low));
+    	
+        while(i<j)
+        {
+         
+         
+         BigInteger lowbig=new BigInteger(unsorted.get(i));
+         BigInteger highbig=new BigInteger(unsorted.get(j));
+     
+          //right
+          while(i<j&& index.compareTo(highbig)<=0)
+          {
+        	  j--;
+        	  highbig=new BigInteger(unsorted.get(j));
+          }  
+          //left
+          while(i<j&&index.compareTo(lowbig)>=0 )
+          {
+        	  i++;
+        	  lowbig=new BigInteger(unsorted.get(i));
+          }
+          if(i<j)	
+          {
+        	  String temp=unsorted.get(i);
+        	  unsorted.set(i, unsorted.get(j));
+        	  unsorted.set(j, temp);
+          }
+        }
+          unsorted.set(low, unsorted.get(j));
+          unsorted.set(i, index.toString());
+          
+          
+          quickSorting(unsorted,low,j-1);
+          quickSorting(unsorted,j+1,high);
+         
+       
+        
+       
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> unsorted = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = SortList.bigSorting(unsorted);
+
+
+
+        bufferedReader.close();
+
+    }
+
 
 }
