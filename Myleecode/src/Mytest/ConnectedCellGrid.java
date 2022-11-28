f0e66b9b6fdaab33bb66cf0f51a1ecd6ef2ebb20
Justsosoly <liaoyigenius@gmail.com>
2022-11-28 14:42:01 +0800

    1128

--- Myleecode/src/Mytest/ConnectedCellGrid.java
+++ Myleecode/src/Mytest/ConnectedCellGrid.java
@@ -1,5 +1,82 @@
 package Mytest;
 
-public class ConnectedCellGrid {
 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class ConnectedCellGridResult {
+
+    /*
+     * Complete the 'connectedCell' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+     */
+
+    public static int connectedCell(List<List<Integer>> matrix) {
+    // Write your code here
+       int result=0;
+       
+       int n=matrix.size();//row
+       int m=matrix.get(0).size();//column
+       
+       for(int i=0;i<n;i++)
+    	   for(int j=0;j<m;j++)
+    	   {
+    		   if(matrix.get(i).get(j)==1)
+    		   {
+    			   
+    		   }
+    		   
+    	   }
+       
+       
+       
+       
+       return result;
+      
+
+    }
+
+}
+
+public class ConnectedCellGrid {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> matrix = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                matrix.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = ConnectedCellGridResult.connectedCell(matrix);
+
+
+        bufferedReader.close();
+    }
 }
